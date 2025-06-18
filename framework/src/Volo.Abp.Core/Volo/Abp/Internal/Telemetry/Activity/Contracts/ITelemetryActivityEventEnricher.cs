5731664e05e117ce7a494623fe3546042a5bd9c6
Emre KARA <emre.crpzz@gmail.com>
2025-06-18 11:55:36 +0300

    Remove `CanExecuteAsync` from `ITelemetryActivityEventEnricher` and add `System.Management` package reference.

--- framework/src/Volo.Abp.Core/Volo/Abp/Internal/Telemetry/Activity/Contracts/ITelemetryActivityEventEnricher.cs
+++ framework/src/Volo.Abp.Core/Volo/Abp/Internal/Telemetry/Activity/Contracts/ITelemetryActivityEventEnricher.cs
@@ -5,7 +5,6 @@ namespace Volo.Abp.Internal.Telemetry.Activity.Contracts;
 public interface ITelemetryActivityEventEnricher
 {
     int ExecutionOrder { get; } 
-    Task<bool> CanExecuteAsync(ActivityContext context);
     Task EnrichAsync(ActivityContext context);
     
 }
